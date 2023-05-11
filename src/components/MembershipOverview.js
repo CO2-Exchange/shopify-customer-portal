9d71a619573ed330ca59ad35a5b80d54f0c7a40e
Frank S <fsalerno@sodasense.com>
2023-05-11 13:44:36 -0500

    More updates

--- src/components/MembershipOverview.js
+++ src/components/MembershipOverview.js
@@ -2,48 +2,35 @@
 // Also houses actions for requesting replacement label and replacement box
 // Will be used to supply discounts (if/when supported)
 
-function MembershipOverview({ address, product, price }) {
+function MembershipOverview({ address, product, price, image }) {
   return (
-    <div class='bg-white col-span-full lg:col-span-8 order-1 p-4 rounded-lg shadow'>
-      <div class='mb-4'>
-        <h3 class='text-xl font-bold text-gray-900'>Membership Overview</h3>
+    <div class="bg-white col-span-full lg:col-span-8 order-1 p-4 rounded-lg shadow">
+      <div class="mb-4">
+        <h3 class="text-xl font-bold text-gray-900">Membership Overview</h3>
       </div>
-      <div class='mb-4'>
-        {address.address1} {address.address2}, {address.city}{' '}
-        {address.provinceCode}, {address.zip}
+      <div class="mb-4">
+        {address.address1} {address.address2}, {address.city} {address.provinceCode}, {address.zip}
       </div>
-      <div class='gap-8 grid grid-cols-12 md:grid-cols-5 md:grid-flow-col md:grid-rows-2'>
-        <img
-          alt='THE 60L CO2 CANISTER'
-          src='https://cdn.shopify.com/s/files/1/0262/9258/4497/products/Box_20and_202_20Canisters_91d61f48-b45a-4790-aa3a-4187e207a813_1024x1024.png?v=1669846782'
-          class='col-span-4 md:col-span-1 md:row-span-2'
-        />
-        <div class='col-span-8 flex flex-wrap justify-between lg:col-span-2 md:col-span-2'>
-          <div class=''>
-            <div class='text-gray-500'>Product</div>
-            <div class='font-bold text-blue-500'>{product}</div>
+      <div class="gap-8 grid grid-cols-12 md:grid-cols-5 md:grid-flow-col md:grid-rows-2">
+        {image ? <img src={image} class="col-span-4 md:col-span-1 md:row-span-2" /> : <></>}
+        <div class="col-span-8 flex flex-wrap justify-between lg:col-span-2 md:col-span-2">
+          <div class="">
+            <div class="text-gray-500">Product</div>
+            <div class="font-bold text-blue-500">{product}</div>
           </div>
-          <div class=''>
-            <div class='text-gray-500'>Total</div>
+          <div class="">
+            <div class="text-gray-500">Total</div>
             <div>{price}</div>
           </div>
         </div>
-        <button class='border-2 border-blue-500 col-span-full md:col-span-2 p-2 rounded-md text-blue-500 place-self-center w-full'>
-          Email Me a Mailing Label
-        </button>
-        <form class='col-span-full md:col-span-2'>
-          <label for='discount-code' class='text-gray-500 text-sm'>
+        <button class="border-2 border-blue-500 col-span-full md:col-span-2 p-2 rounded-md text-blue-500 place-self-center w-full">Email Me a Mailing Label</button>
+        <form class="col-span-full md:col-span-2">
+          <label for="discount-code" class="text-gray-500 text-sm">
             Apply Discount Code To Your Next Refill
           </label>
-          <input
-            id='discount-code'
-            type='text'
-            class='bg-gray-50 rounded-md shadow-inner w-full'
-          />
+          <input id="discount-code" type="text" class="bg-gray-50 rounded-md shadow-inner w-full" />
         </form>
-        <button class='border-2 border-blue-500 col-span-full md:col-span-2 p-2 rounded-md text-blue-500 place-self-center w-full'>
-          Send Me a Refill Box
-        </button>
+        <button class="border-2 border-blue-500 col-span-full md:col-span-2 p-2 rounded-md text-blue-500 place-self-center w-full">Send Me a Refill Box</button>
       </div>
     </div>
   );
--- src/components/Subscription.js
+++ src/components/Subscription.js
@@ -15,29 +15,25 @@ function Subscription(props) {
   const [subscriptionAddress, setSubscriptionAddress] = useState({});
   const [paymentMethod, setPaymentMethod] = useState({});
   const [loading, setLoading] = useState(true);
-  const [subscriptionProduct, setSubscriptionProduct] = useState("")
+  const [subscriptionProduct, setSubscriptionProduct] = useState('');
+  const [variantImage, setVariantImage] = useState('');
   const [subscriptionProductAmount, setProductAmount] = useState(null);
-  const navigator = useNavigate()
-  const pathPrefix = devMode? "/subscription/" : "/apps/fillstation/web/subscription/";
+  const navigator = useNavigate();
+  const pathPrefix = devMode ? '/subscription/' : '/apps/fillstation/web/subscription/';
 
-  // In dev mode, assign local variables to mockjson. 
+  // In dev mode, assign local variables to mockjson.
   // In production, fetch contracts and assignVariables
   useEffect(() => {
-    if(devMode)
-    assignVariables(contractsDetailsMockJson)
-    else
-    fetchSubscription(window.location.pathname.substring(
-      window.location.pathname.lastIndexOf('/') + 1
-    ));
+    if (devMode) assignVariables(contractsDetailsMockJson);
+    else fetchSubscription(window.location.pathname.substring(window.location.pathname.lastIndexOf('/') + 1));
   }, []);
 
-  function navigateToSubscription(e){
-    navigator(pathPrefix+e.target.value)
-    fetchSubscription(e.target.value)
+  function navigateToSubscription(e) {
+    navigator(pathPrefix + e.target.value);
+    fetchSubscription(e.target.value);
   }
-  function assignVariables(respJson){
-    var cleanedResponse =
-      respJson.subscriptionContract.orders.edges;
+  function assignVariables(respJson) {
+    var cleanedResponse = respJson.subscriptionContract.orders.edges;
     var filteredOrders = [];
     //map graphql response to cleaner array structure
     for (const order of cleanedResponse) {
@@ -45,49 +41,39 @@ function Subscription(props) {
     }
     setOrders(filteredOrders);
     //Set subscription product amount to the most recent orders price
-    setProductAmount(respJson.subscriptionContract.orders.edges[0].node.originalTotalPriceSet.shopMoney.amount);
+    setProductAmount(respJson.subscriptionContract.lines.edges[0].node.currentPrice.amount);
     //Set subscription product amount to the most recent orders first product
-    setSubscriptionProduct(respJson.subscriptionContract.orders.edges[0].node.lineItems.edges[0].node.name);
+    setSubscriptionProduct(respJson.subscriptionContract.lines.edges[0].node.title);
 
-    setSubscriptionAddress(
-      respJson.subscriptionContract.deliveryMethod.address
-    );
-    setPaymentMethod(
-      respJson.subscriptionContract.customerPaymentMethod
-    );
+    if (respJson.subscriptionContract.lines.edges[0].node.variantImage.url) setVariantImage(respJson.subscriptionContract.lines.edges[0].node.variantImage.url);
+
+    setSubscriptionAddress(respJson.subscriptionContract.deliveryMethod.address);
+    setPaymentMethod(respJson.subscriptionContract.customerPaymentMethod);
     setLoading(false);
   }
   async function fetchSubscription(id) {
-    setLoading(true)
-    var resp = await fetch(
-      `/apps/fillstation/api/v1/customer/subscription?subscriptionContractId=${id}`,
-      {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
+    setLoading(true);
+    var resp = await fetch(`/apps/fillstation/api/v1/customer/subscription?subscriptionContractId=${id}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
       }
-    );
+    });
     var respJson = await resp.json();
-    assignVariables(respJson)
+    assignVariables(respJson);
   }
 
   function SubscriptionPage() {
     if (loading) return <h1>loading</h1>;
     return (
-      <main class='gap-4 lg:gap-8 grid grid-cols-1 lg:grid-cols-12 p-4 lg:p-8'>
-        <nav class='col-span-full flex justify-between' aria-label='Breadcrumb'>
-          <ol class='inline-flex items-center space-x-1 md:space-x-3'>
-            <select
-              class='border border-gray-300 text-gray-900 text-sm rounded-r-lg border-l-gray-100 dark:border-l-gray-700 border-l-2 focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'
-              name='provinceCode'
-              onChange={navigateToSubscription}
-            >
-              {props.contracts.map((contract) => {
+      <main class="gap-4 lg:gap-8 grid grid-cols-1 lg:grid-cols-12 p-4 lg:p-8">
+        <nav class="col-span-full flex justify-between" aria-label="Breadcrumb">
+          <ol class="inline-flex items-center space-x-1 md:space-x-3">
+            <select class="border border-gray-300 text-gray-900 text-sm rounded-r-lg border-l-gray-100 dark:border-l-gray-700 border-l-2 focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" name="provinceCode" onChange={navigateToSubscription}>
+              {props.contracts.map(contract => {
                 return (
-                  <option key={contract.id} value={contract.id.substring(
-                    contract.id.lastIndexOf('/') + 1)}>
-                    {contract.deliveryMethod.address.address1}{' '}{contract.lines.edges[0].node.title}
+                  <option key={contract.id} value={contract.id.substring(contract.id.lastIndexOf('/') + 1)}>
+                    {contract.deliveryMethod.address.address1} {contract.lines.edges[0].node.title}
                   </option>
                 );
               })}
@@ -95,21 +81,11 @@ function Subscription(props) {
           </ol>
         </nav>
 
-        <div class='grid grid-cols-12 items-start lg:block lg:col-span-8 lg:space-y-8 space-y-4'>
-          <MembershipOverview
-            address={subscriptionAddress}
-            product={subscriptionProduct}
-            price={subscriptionProductAmount}
-          ></MembershipOverview>
+        <div class="grid grid-cols-12 items-start lg:block lg:col-span-8 lg:space-y-8 space-y-4">
+          <MembershipOverview address={subscriptionAddress} product={subscriptionProduct} price={subscriptionProductAmount} image={variantImage}></MembershipOverview>
           <ExchangeHistory orders={orders}></ExchangeHistory>
         </div>
-        <AccountInformation
-          subscriptionAddress={subscriptionAddress}
-          paymentMethod={paymentMethod}
-          contractId={window.location.pathname.substring(
-            window.location.pathname.lastIndexOf('/') + 1
-          )}
-        ></AccountInformation>
+        <AccountInformation subscriptionAddress={subscriptionAddress} paymentMethod={paymentMethod} contractId={window.location.pathname.substring(window.location.pathname.lastIndexOf('/') + 1)}></AccountInformation>
       </main>
     );
   }
